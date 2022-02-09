5b2e9558443d5ce6be68d8aeb28d28091db818c3
ᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠ <29128703+milksense@users.noreply.github.com>
2022-02-09 17:53:12 +0300

    Minor fixes & improvements
    :v:

--- apps/web/pages/_document.tsx
+++ apps/web/pages/_document.tsx
@@ -2,7 +2,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { CssBaseline } from '@nextui-org/react';
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(ctx: any) {
     const initialProps = await Document.getInitialProps(ctx);
     return {
       ...initialProps,
@@ -13,7 +13,12 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>{CssBaseline.flush()}</Head>
+        <Head>
+          {CssBaseline.flush()}
+          <title>I guess your city</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+          <link rel="icon shortcut" type="image/x-icon" href="https://assets.vercel.com/image/upload/q_auto/front/favicon/vercel/favicon.ico" />
+        </Head>
         <body>
           <Main />
           <NextScript />
