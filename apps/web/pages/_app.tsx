61f1c3d790c37e0349414a23251f032fabb8b9b0
ᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠ <29128703+milksense@users.noreply.github.com>
2022-02-09 19:53:37 +0300

    Minor fixes & improvements
    :v:

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,13 +1,16 @@
-// 1. import `NextUIProvider` component
 import { NextUIProvider } from '@nextui-org/react';
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }) {
-  return (
-    // 2. Use at the root of your app
+const myApp = ({ Component, pageProps }) => (
+  <>
+    <Head>
+      <title>I guess your city</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    </Head>
     <NextUIProvider>
       <Component {...pageProps} />
     </NextUIProvider>
-  );
-}
+  </>
+);
 
-export default MyApp;
+export default myApp;
--- apps/web/pages/_document.tsx
+++ apps/web/pages/_document.tsx
@@ -15,8 +15,6 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           {CssBaseline.flush()}
-          <title>I guess your city</title>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
           <link rel="icon shortcut" type="image/x-icon" href="https://assets.vercel.com/image/upload/q_auto/front/favicon/vercel/favicon.ico" />
         </Head>
         <body>
